68c226176b2343cb253dc3bac273ec004aca268c
rimkusaurimas <labas@aurimasrimkus.lt>
2021-08-05 00:39:07 +0300

    feeding pair names from objects

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,50 +9,55 @@ $(document).ready(function () {
 
     const clpwPair = {
 
-        btcusdt: {
-            name: "!BTC/USDT!",
+        pair1: {
+            name: "!BTC/USDT",
             percent: 9.2,
             dollarValue: 42.234,
             value: 42.234
         },
 
-        ethusdt: {
-            name: "ETH/USDT",
+        pair2: {
+            name: "!ETH/USDT",
             percent: -3.02,
             dollarValue: 1800.23,
             value: 1800.23
         },
 
-        axsbnb: {
-            name: "AXS/USDT",
+        pair3: {
+            name: "!AXS/USDT",
             percent: 15,
             dollarValue: 39.08,
             value: 0.1032
         },
 
-        dogeusdt: {
-            name: "DOGE/USDT",
+        pair4: {
+            name: "!DOGE/USDT",
             percent: -4.02,
             dollarValue: 0.3014,
             value: 0.3014
         },
 
-        lunabnb: {
-            name: "LUNA/BNB",
+        pair5: {
+            name: "!LUNA/BNB",
             percent: 1,
             dollarValue: 14.94,
             value: 0.04465
         },
 
-        wintrx: {
-            name: "WIN/TRX",
+        pair6: {
+            name: "!WIN/TRX",
             percent: 5.58,
             dollarValue: 0.00038,
             value: 0.00568
         }
     };
 
-    $("#percPairName1").replaceWith(clpwPair.btcusdt.name);
+    $("#percPairName1").replaceWith(clpwPair.pair1.name);
+    $("#percPairName2").replaceWith(clpwPair.pair2.name);
+    $("#percPairName3").replaceWith(clpwPair.pair3.name);
+    $("#percPairName4").replaceWith(clpwPair.pair4.name);
+    $("#percPairName5").replaceWith(clpwPair.pair5.name);
+    $("#percPairName6").replaceWith(clpwPair.pair6.name);
 
     
 
