371f1d650034e0343f403a42830c5c494a12515b
rimkusaurimas <labas@aurimasrimkus.lt>
2021-08-17 00:27:01 +0300

    js objects filled

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,42 +3,84 @@ $(document).ready(function () {
     // Crypto Live Preview Widget
     const clpwPairs = {
         pair1: {
-            name: "!BTC/USDT",
+            name: "BTC/USDT",
             percent: 9.2,
             dollarValue: 42.234,
             value: 42.234
         },
 
         pair2: {
-            name: "!ETH/USDT",
+            name: "ETH/USDT",
             percent: -3.02,
             dollarValue: 1800.23,
             value: 1800.23
         },
 
         pair3: {
-            name: "!AXS/USDT",
+            name: "AXS/USDT",
             percent: 15,
             dollarValue: 39.08,
             value: 0.1032
         },
 
         pair4: {
-            name: "!DOGE/USDT",
+            name: "DOGE/USDT",
             percent: -4.02,
             dollarValue: 0.3014,
             value: 0.3014
         },
 
         pair5: {
-            name: "!LUNA/BNB",
+            name: "LUNA/BNB",
             percent: 1,
             dollarValue: 14.94,
             value: 0.04465
         },
 
         pair6: {
-            name: "!WIN/TRX",
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+
+        pair7: {
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+
+        pair8: {
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+
+        pair9: {
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+        
+        pair10: {
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+
+        pair11: {
+            name: "WIN/TRX",
+            percent: 0,
+            dollarValue: 0.00038,
+            value: 0.00568
+        },
+
+        pair12: {
+            name: "WIN/TRX",
             percent: 0,
             dollarValue: 0.00038,
             value: 0.00568
@@ -57,6 +99,9 @@ $(document).ready(function () {
             } else {
                 procentai.css("color", "rgb(14, 203, 129)");
             }
+            pair.find(".percentage-pair-name").append(clpwPairs[key].name);
+            pair.find(".price-small").append(clpwPairs[key].dollarValue + "$");
+            pair.find(".price-big").append(clpwPairs[key].value);
         }
     }
 
