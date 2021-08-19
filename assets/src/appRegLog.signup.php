8e4f926c8bfdc253bd45234a096168ed123690e6
rimkusaurimas <labas@aurimasrimkus.lt>
2021-08-20 01:37:00 +0300

    signup  - everything done except nothing in db

--- assets/src/appRegLog.signup.php
+++ assets/src/appRegLog.signup.php
@@ -6,41 +6,46 @@ if (isset($_POST['submit'])) {
     $email = mysqli_real_escape_string($mysqli, $_POST['email']);
     $uid = mysqli_real_escape_string($mysqli, $_POST['uname']);
     $pwd = mysqli_real_escape_string($mysqli, $_POST['pw']);
+    $pwd2 = mysqli_real_escape_string($mysqli, $_POST['pw2']);
     $terms = mysqli_real_escape_string($mysqli, $_POST['check']);
 
     if (empty($terms)) {
         header("Location: ../../?signup=termsnotmet");
         exit();
     } else {
-        if (empty($name) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+        if (empty($name) || empty($email) || empty($uid) || empty($pwd) || empty($pwd2)) {
             header("Location: ../../?signup=empty");
             exit();
         } else {
             // Ar teisingai ivesti simboliai
-            if (!preg_match("/^[a-zA-Z]*$/", $name) || !preg_match("/^[a-zA-Z]*$/", $last)) {
-                header("Location: ../signup.php?singup=invalid");
+            if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+                header("Location: ../../?signup=invalid");
                 exit();
             } else {
                 // Ar galiojantis email formatas
                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    header("Location: ../signup.php?singup=invalidemail");
+                    header("Location: ../../?signup=invalidemail");
                     exit();
                 } else {
                     // Ar username nenaudojamas
-                    $sql = "SELECT * FROM users WHERE user_uid = '$uid'";
-                    $result = mysqli_query($conn, $sql);
+                    $sql = "SELECT * FROM loginreg WHERE user_username = '$uid'";
+                    $result = mysqli_query($mysqli, $sql);
                     $result_check = mysqli_num_rows($result);
                     if ($result_check > 0) {
-                        header("Location: ../signup.php?singup=usertaken");
+                        header("Location: ../../?signup=usertaken");
                         exit();
                     } else {
-                        // Sukuriame hash slaptazodi
-                        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-                        // Ikeliame vartotoja i DB
-                        $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd')";
-                        mysqli_query($conn, $sql);
-                        header("Location: ../signup.php?signup=success");
-                        exit();
+                        if (!$pwd = $pwd2) {
+                            header("Location: ../../?signup=passwordnomatch");
+                            exit();
+                        } else {
+                            // Sukuriame hash slaptazodi
+                            $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+                            // Ikeliame vartotoja i DB
+                            mysqli_query($mysqli, "INSERT INTO loginreg (user_name, user_email, user_username, user_pw) VALUES ('$name'), ('$email'), ('$uid'), ('$pwd'), ('$terms')");
+                            header("Location: ../../?signup=usercreated");
+                            exit();
+                        }
                     }
                 }
             }
